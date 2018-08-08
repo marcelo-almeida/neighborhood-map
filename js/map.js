e6a5696f10c0bcf7de46eab3622e48e0a29a412d
Marcelo Augusto de Almeida <marcelo777_augusto@hotmail.com>
2018-08-08 20:38:58 -0300

    use new helpers and added click events to each marker

--- js/map.js
+++ js/map.js
@@ -1,4 +1,8 @@
 var map;
+var markerList = [];
+let defaultIcon;
+let selectedIcon;
+const placesMap = getPlaces();
 
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
@@ -8,28 +12,72 @@ function initMap() {
         mapTypeControl: false
     });
 
-    const defaultIcon = makeMarkerIcon('ff6347');
-    const places = getPlaces();
-    places().forEach(function(place){
-        const location = place.location;
-        const title = place.title;
+    defaultIcon = makeMarkerIcon('7f3123');
+    selectedIcon = makeMarkerIcon('ff6347');
+    var infoWindow = new google.maps.InfoWindow();
+
+    placesMap().forEach(function (place) {
+        const location = place.location();
+        const title = place.title();
         const marker = new google.maps.Marker({
             position: location,
             map: map,
             title: title,
             animation: google.maps.Animation.DROP,
             icon: defaultIcon
-          });
+        });
+
+        marker.addListener('click', function () {
+            resetAllMarkers();
+            this.setIcon(selectedIcon);
+            showInfoWindow(this, infoWindow);
+            centerMarkers();
+        });
+        markerList.push(marker);
     });
+}
+
+function showInfoWindow(marker, infoWindow) {
+    if (infoWindow.marker != marker) {
+        infoWindow.marker = marker;
+        infoWindow.setContent('<div>' + marker.title + '</div>');
+        infoWindow.open(map, marker);
+        infoWindow.addListener('closeclick', function () {
+            infoWindow.marker = null;
+        });
+    }
+}
+
+function centerMarkers() {
+    var bounds = new google.maps.LatLngBounds();
+    markerList.forEach(function(marker){
+        bounds.extend(marker.position);
+    });
+    map.fitBounds(bounds);
+}
+
+function resetAllMarkers() {
+    markerList.forEach(function (maker) {
+        maker.clicked = false;
+        maker.setIcon(defaultIcon);
+    })
+}
+
+function selectMarkerFromList(title) {
+    markerList.forEach(function (marker) {
+        if (marker['title'] === title) {
+            google.maps.event.trigger(marker, 'click');
+        }
+    })
 }
 
 function makeMarkerIcon(markerColor) {
     var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21,34));
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21, 34));
     return markerImage;
-  }
+}
