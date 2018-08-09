e607193b5b454119ebd81d01e8815e9d5668f0ea
Marcelo Augusto de Almeida <marcelo777_augusto@hotmail.com>
2018-08-09 00:51:44 -0300

    implement search

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
-var ViewModel = function(){
+var ViewModel = function () {
     self = this;
     this.places = placesMap;
+    this.query = ko.observable("");
 
+    this.searchPlaces = function () {
+        const query = self.query().toLowerCase();
+        let matchList = [];
+        self.places().forEach(place => {
+            const pos = place.title().toLowerCase().search(query);
+            if (pos >= 0) {
+                place.visible(true);
+            } else {
+                place.visible(false);
+            }
+            place.selected(false);
+        });
+        updateMarkers();
+        
+    }
 }
 
-ko.applyBindings(new ViewModel());
+var vm = new ViewModel();
+vm.query.subscribe(vm.searchPlaces);
+ko.applyBindings(vm);
